ffb04f94db8b42948b60522847797ec6fa30be4d
michaelPro89 <47365331+michaelPro89@users.noreply.github.com>
2021-11-07 14:51:00 +0100

    Create FileScrapper.py

---
+++ FileScrapper.py
@@ -0,0 +1,71 @@
+import shutil
+import fileinput
+import os
+
+
+class FileScrapper:
+
+    def __init__(self):
+        pass
+
+    def copy(self, file, file_destiny):
+        shutil.copy("./dir/vagrant3/{file}".format(file=file), file_destiny)
+
+    def edit_files(self, file1):
+        print("Im inside editing files")
+        for line in fileinput.FileInput(file1, inplace=1):
+            if 'VM_OWNER="PLEASE-CHANGE-ME"' in line:
+                # line = line.rstrip()
+                line = line.replace("PLEASE-CHANGE-ME", 'michal.switala')
+                print line
+            else:
+                print("test_line")
+
+    def create_Bootstrap(self, original, new, username, userid):
+
+        input = open(original, "rt")
+
+        # save new copy of file over here :
+        output = open(new, "wt")
+
+        for line in input:
+
+            if 'VM_OWNER="PLEASE-CHANGE-ME"' in line:
+
+                output.write(line.replace('PLEASE-CHANGE-ME', username))
+
+            elif 'SERVER_ID="PLEASE-CHANGE-ME"' in line:
+
+                output.write(line.replace('PLEASE-CHANGE-ME', str(userid)))
+
+            else:
+                output.write(line)
+
+        input.close()
+        output.close()
+
+    def create_Vagrantfile(self, original, new, sync_folder_path):
+
+        input = open(original, "rt")
+
+        # create a new copy of file over here :
+        output = open(new, "wt")
+
+        for line in input:
+
+            if 'devs.synced_folder "~/src", "/dir/vagrant/my_src"' in line:
+
+                output.write(line.replace('~/src', sync_folder_path))
+
+            else:
+                output.write(line)
+
+        input.close()
+        output.close()
+
+    def deleteFile(self, filepath):
+        os.remove(filepath)
+
+    def renameFile(self, oldfile, newfile):
+
+        os.rename(oldfile, newfile)
